d46eba15d183fc0ab5f31afad823c44cbe92a8a1
sreekar339339 <sreekar339@gmail.com>
2025-07-02 23:48:58 +0530

    Update ESLint configuration for v9 - Remove old .eslintrc.js - Add new eslint.config.js with ESM format - Install @eslint/js and globals packages

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        chrome: 'readonly'
+      },
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+      'no-console': 'off',
+      'no-useless-escape': 'off',
+    },
+    ignorePatterns: ['node_modules/', 'dist/', 'build/'],
+  }
+];
